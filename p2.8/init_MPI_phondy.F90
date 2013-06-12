ef9619a3cf4701d0de882bf9908fe613ea8aba15
marinica <marinica@196e1088-85ce-11df-b670-1114daa2ea76>
2013-06-12 16:40:34 +0000

    I forgot one file from the previous commit.
    git-svn-id: svn://132.166.68.101/NDM@163 196e1088-85ce-11df-b670-1114daa2ea76

---
+++ p2.8/init_MPI_phondy.F90
@@ -0,0 +1,34 @@
+module mod_mpi_phondy 
+ use mpi
+     integer, dimension(MPI_STATUS_SIZE) :: statut
+     integer :: nb_procsph,codeph
+end module mod_mpi_phondy
+
+
+
+
+subroutine init_mpi_phondy()
+
+  use mpi
+  use mod_mpi_phondy
+  use gen_com_m , ONLY: rangph
+  implicit none
+
+  ! Routine d'initialisation de MPI pour le code NDM
+
+
+  !--------------------------------------------------
+  !Variables de la routine
+
+  !--------------------------------------------------
+  !Variables locales
+
+  !--------------------------------------------------
+  !Corps de la routine
+
+call MPI_INIT (codeph)
+call MPI_COMM_SIZE(MPI_COMM_WORLD,nb_procsph, codeph)
+call MPI_COMM_RANK(MPI_COMM_WORLD,rangph,codeph)
+
+
+end subroutine init_mpi_phondy
