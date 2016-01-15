94f0a6f0309164a4ad4a811fdbf1e4d6605910a3
marinica <marinica@196e1088-85ce-11df-b670-1114daa2ea76>
2016-01-15 14:46:00 +0000

    git-svn-id: svn://132.166.68.101/NDM@298 196e1088-85ce-11df-b670-1114daa2ea76

--- p2.8/divid.F90
+++ p2.8/divid.F90
@@ -84,7 +84,7 @@ subroutine divid (appel)
      if (izonr<2) then
         write (6, *) 'trop petite boite !!!'
         !cosboite  stop
-#if(PHONDY || PARAPH || MAB)
+#if(PHONDY || PARAPH || MAB || ML || PARAML)
         write (6, *) 'trop petite boite !!!'
 #else
   if (lrctest) then
@@ -289,7 +289,7 @@ subroutine divid (appel)
      if (izonr2<1) then
         write (6, *) rang,'trop petite boite pour rvois !!!'
         !cosboite   call arret_ndm
-#if(PHONDY || PARAPH || MAB)
+#if(PHONDY || PARAPH || MAB || ML || PARAML)
 
         write (6, *) rang,'trop petite boite pour rvois !!!'
 #else 
