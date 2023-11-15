1a245a12df0cc95559d658b7b8a32c0973b3d6f7
Jean-Paul Crocombette <jpcrocombette@cea.fr>
2023-11-15 15:16:49 +0100

    compilation gatsby de version CVW du printemps

--- scripts/jinstall.para.gastby.bash
+++ scripts/jinstall.para.gastby.bash
@@ -1,13 +1,18 @@
 #!/bin/bash -f
+#source ~/.bashrc
+
 export NDM_ROODIR=/home/croc/NDM/ndm2021_cv
 cd ${NDM_ROODIR}
 
 export NDM_SRCDIR=${NDM_ROODIR}/NDM
-export NDM_BUIDIR=${NDM_ROODIR}/ndm_build_ifort
-export NDM_INSDIR=${NDM_ROODIR}/ndm_install_ifort
+export NDM_BUIDIR=${NDM_ROODIR}/ndm_build_ifort_para
+export NDM_INSDIR=${NDM_ROODIR}/ndm_install_ifort_para
 
 export PATH=${NDM_SRCDIR}/scripts:${PATH}  # to get NMD/scripts
 envs -p NDM_
+
+
+
 
 function f_cmake_clean_doc {
   rm -rf ${NDM_SRCDIR}/docs/doc_2023/_build
@@ -18,42 +23,49 @@ function f_cmake_clean_doc {
 
 function f_cmake_clean {
   f_cmake_clean_doc
-  cd ${NDM_ROODIR}
+#  cd ${NDM_ROODIR}
   rm -rf ${NDM_BUIDIR}
   rm -rf ${NDM_INSDIR}
   mkdir ${NDM_BUIDIR}
-  cd ${NDM_BUIDIR}   # important
+#  cd ${NDM_BUIDIR}   # important
+
 }
 
 # preset.cmakes files makes this important fortran compiler choice useless
 unset FC
+
+# which mpif90
+
+export MPI_HOME=/home/soft/intel/oneapi/mpi/2021.6.0/bin/
+
 cd ${NDM_ROODIR}
 
-f_cmake_clean   # important
+#f_cmake_clean   # important
 
- cd ${NDM_BUIDIR}   # important
+cd ${NDM_BUIDIR}   # important
 
 
-###### IFORT serial
-cmake -C ${NDM_SRCDIR}/cmake_files/ndm_preset_ifort_serial.cmake -S ${NDM_SRCDIR}
-
+cmake -C ${NDM_SRCDIR}/cmake_files/ndm_preset_oneapi_parallel.cmake -S ${NDM_SRCDIR}
 
 cmake --build . --target all --parallel 4
 #cmake --build . --target install
 #cmake --build . --target test
 
+
 # checks
-tree -d ${NDM_BUIDIR} # ../ndm_build_gnu/
+tree -d ${NDM_BUIDIR} 
+#tree -d ${NDM_INSDIR} 
+# tree -d ../ndm_*gnu
 #ldd ${NDM_BUIDIR}/bin/*
+
 
 # # # make an integration example...
  rm -rf  ./tmp
- cp -rf ${NDM_SRCDIR}/examples/ndm_lammps_serial ./tmp
+ cp -rf ${NDM_SRCDIR}/examples/ndm_mpi ./tmp
  cd ./tmp
 
-#ndm_preset_gnu_serial #????#
- ${NDM_BUIDIR}/bin/ndm_main.exe
+
+#parallel
+ mpirun -n 4 ${NDM_BUIDIR}/bin/ndm_main.exe
 # ---> ok
-
-
 
