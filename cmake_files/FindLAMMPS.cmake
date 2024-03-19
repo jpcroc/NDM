22815836780bb90f794fa531afdf7fcff88a08fe
DAUBIN Jonathan <jd270899@is246153.intra.cea.fr>
2024-03-19 17:17:29 +0100

    cmake update: MKL for TOPAZE/IRENE and Lammps

--- cmake_files/FindLAMMPS.cmake
+++ cmake_files/FindLAMMPS.cmake
@@ -1,35 +1,115 @@
+#[[
+FindLAMMPS
+
+Tries to find lammps using pkg-config and some more hazardous ways.
+Called with Find_package(LAMMPS).
+
+PKG-CONFIG
+----------
+To use pkg-config successfully make sure the .pc file (usually liblammps.pc) is included in the PKG_CONFIG_PATH.
+The .pc file name depends on lammps library name, see below.
+
+LAMMPS_LIBRARY_NAME
+-------------------
+Variable to set in Cmake or as an environment variable. By default "lammps".
+Depends on <LAMMPS_MACHINE> custom parameter which may be set at lammps compilation and is related to the executable name:
+executable name : lmp_<LAMMPS_MACHINE>
+library name: lammps_<LAMMPS_MACHINE>
+For example "lammps_mpi" is popular for parallel lammps but there is no convention.
+
+#]]
+################################################
+# Functions and Macros definitions
+################################################
+
+# Function to extract definition variables from a list of cflags
+# Definitions variables are stored in RESULT_GET_DEFINITION variable
+function (get_definition TMP_FLAGS)
+    set(RESULT_GET_DEFINITION "")
+    foreach(TMP_FLAG IN ITEMS ${TMP_FLAGS})
+        if ( ${TMP_FLAG} MATCHES "^-D" ) # if flag starts with -D
+            string(SUBSTRING ${TMP_FLAG} 2 -1 TMP_DEF) # ignore the two first characters
+            list(APPEND RESULT_GET_DEFINITION ${TMP_DEF})
+        endif()
+    endforeach()
+endfunction(get_definition)
+
+# Macro to try looking for lammps using pkg-config
+macro(pkg_search)
+    find_package(PkgConfig QUIET)
+    if (${PKG_CONFIG_FOUND})
+        pkg_check_modules(LAMMPS "lib${LAMMPS_LIBRARY_NAME}")
+        get_definition("${LAMMPS_CFLAGS}")
+        list(APPEND LAMMPS_DEFINITIONS ${RESULT_GET_DEFINITION})
+    endif()
+endmacro(pkg_search)
+
+# Macro to try finding lammps library in LD_LIBRARY_PATH environment variable
+macro (ld_library_search)
+    string(REPLACE ":" ";" TMP_LD_PATH $ENV{LD_LIBRARY_PATH})
+    find_library(TMP_PATH_TO_LIBRARY ${LAMMPS_LIBRARY_NAME} HINTS ${TMP_LD_PATH})
+    if (NOT TMP_PATH_TO_LIBRARY STREQUAL "TMP_PATH_TO_LIBRARY-NOTFOUND")
+        set(LAMMPS_FOUND ON)
+        cmake_path(REMOVE_FILENAME TMP_PATH_TO_LIBRARY)
+        list(APPEND LAMMPS_LIBRARY_DIRS ${TMP_PATH_TO_LIBRARY})
+        list(APPEND LAMMPS_INCLUDE "${TMP_PATH_TO_LIBRARY}/../include" )
+    endif()
+endmacro(ld_library_search)
+
+################################################
+# Sequential script
+################################################
+
+# Standard arguments for Find_package function
 include(FindPackageHandleStandardArgs)
 
-# order matters
-set(LAMMPS_MODULE_NAME lammps_mpi)
-set(LAMMPS_DEFINITIONS "" PARENT_SCOPE)
-
-find_package(PkgConfig QUIET)
-if (${PKG_CONFIG_FOUND})
-    pkg_check_modules(LAMMPS "lib${LAMMPS_MODULE_NAME}")
-#    set(LAMMPS_LIBRARIES ${PKGC_MKL_LIBRARIES})
-#    set(LAMMPS_LINK_LIBRARIES ${PKGC_MKL_LINK_LIBRARIES})
-#    set(LAMMPS_LIBRARY_DIRS ${PKGC_MKL_LIBRARY_DIRS})
-#    set(LAMMPS_LDFLAGS ${PKGC_MKL_LDFLAGS})
-#    set(LAMMPS_CFLAGS ${PKGC_MKL_CFLAGS})
-#    set(LAMMPS_INCLUDE_DIRS ${PKGC_MKL_INCLUDE_DIRS})
-#    set(LAMMPS_VERSION ${PKGC_MKL_VERSION})
+# Initialize LAMMPS_LIBRARY_NAME
+set(LAMMPS_LIBRARY_NAME "lammps" CACHE STRING "Name of the lammps library")
+if (DEFINED ENV{LAMMPS_LIBRARY_NAME})
+    set(LAMMPS_LIBRARY_NAME $ENV{LAMMPS_LIBRARY_NAME})
 endif()
+set(LAMMPS_DEFINITIONS "")
 
-if (NOT LAMMPS_FOUND) 
-    string(REPLACE ":" ";" TMP_LD_PATH $ENV{LD_LIBRARY_PATH})
-    find_library(TMP_PATH_TO_LIBRARY ${LAMMPS_MODULE_NAME} HINTS ${TMP_LD_PATH})
-    if (NOT TMP_PATH_TO_LIBRARY STREQUAL "TMP_PATH_TO_LIBRARY-NOTFOUND")
-        cmake_path(REMOVE_FILENAME TMP_PATH_TO_LIBRARY )
-        list(APPEND LAMMPS_LIBRARY_DIRS ${TMP_PATH_TO_LIBRARY} )
-        list(APPEND LAMMPS_INCLUDE "${TMP_PATH_TO_LIBRARY}/../include" )
-        
-    endif()
-    list(APPEND LAMMPS_LIBRARIES "${LAMMPS_MODULE_NAME}")
+# Try to find Lammps with pkg-config
+pkg_search()
+
+if (NOT LAMMPS_FOUND)
+    # look for lammps libraries in LD_LIBRARY_PATH
+    ld_library_search()  
+    list(APPEND LAMMPS_LIBRARIES "${LAMMPS_LIBRARY_NAME}")
     list(APPEND LAMMPS_DEFINITIONS "LAMMPS_SMALLBIG")
 endif()
 
+# correct some include directories
+foreach (TMP_DIR IN ITEMS ${LAMMPS_INCLUDE_DIRS})
+    if (EXISTS "${TMP_DIR}/lammps")
+        list(APPEND LAMMPS_INCLUDE_DIRS "${TMP_DIR}/lammps")
+    endif()
+endforeach()
 
+# search for extra libraries
+set(LAMMPS_MODULES_LIST     # List of possible extra libraries for lammps
+    meam
+    voro++
+    gsl
+    plumed
+    milady
+    lammps_atc_mpi
+    lammps_qmmm_mpi
+    colvars
+    reax
+    poems
+    awpmd
+)
+
+if (NOT TMP_PATH_TO_LIBRARY STREQUAL "TMP_PATH_TO_LIBRARY-NOTFOUND")
+    foreach (EXTRA_LIB IN ITEMS ${LAMMPS_MODULES_LIST})
+        find_library(TMP_PATH_TO_EXTRA_LIB ${EXTRA_LIB} HINTS ${TMP_PATH_TO_LIBRARY})
+        if (NOT TMP_PATH_TO_EXTRA_LIB STREQUAL "TMP_PATH_TO_EXTRA_LIB-NOTFOUND")
+            list(APPEND LAMMPS_LIBRARIES ${EXTRA_LIB})
+        endif()
+    endforeach()
+endif()
 
 # required variables for find_package()
 find_package_handle_standard_args( LAMMPS 
@@ -41,3 +121,4 @@ set(LAMMPS_LINK_LIBRARIES ${LAMMPS_LINK_LIBRARIES} PARENT_SCOPE)
 set(LAMMPS_CFLAGS ${LAMMPS_CFLAGS} PARENT_SCOPE)
 set(LAMMPS_LDFLAGS ${LAMMPS_LDFLAGS} PARENT_SCOPE)
 set(LAMMPS_INCLUDE_DIRS ${LAMMPS_INCLUDE_DIRS} PARENT_SCOPE)
+set(LAMMPS_DEFINITIONS ${LAMMPS_DEFINITIONS} PARENT_SCOPE)
--- cmake_files/compile_with_LAMMPS.cmake
+++ cmake_files/compile_with_LAMMPS.cmake
@@ -2,69 +2,50 @@
 #
 #   MODULE TO FIND LAMMPS LIBRARY
 #
-#   Make sure lammps.pc is included in the PKG_CONFIG_PATH or
-#   indicate lammps path manually using LAMMPS_PATH and lammps
-#   ldflags and cflags using LAMMPS_FLAGS.
-#
+#   LAMMPS_ROOT
 #
 ############
 
 
 function (compile_with_LAMMPS)
-    
+
+    # Determine library name
+    find_program(VAR_LMP_MPI lmp_mpi)   # check if lmp_mpi executable is available
+    find_program(VAR_LMP lmp)
+    if (NOT ${VAR_LMP_MPI} STREQUAL "VAR_LMP_MPI-NOTFOUND") # lammps mpi is available
+        set(lammps_mpi ON)
+    else()
+        set(lammps_mpi OFF)
+    endif()
+    if (NOT ${VAR_LMP} STREQUAL "VAR_LMP-NOTFOUND") # lammps serial is available
+        set(lammps_serial ON)
+    else()
+        set(lammps_serial OFF)
+    endif()
+    if ("MPI" IN_LIST ${PROJECT_NAME}_PACKAGE_LIST) # project uses mpi
+        set(project_mpi ON)
+    else()
+        set(project_mpi OFF)
+    endif()
+    if (${project_mpi} AND ${lammps_mpi}) # if project uses mpi and lammps_mpi is available
+        set(LAMMPS_LIBRARY_NAME "lammps_mpi" CACHE STRING "Name of the lammps library")
+    elseif(${lammps_mpi} AND NOT ${lammps_serial}) # project doesn't use mpi but only lammps_mpi is available
+        set(LAMMPS_LIBRARY_NAME "lammps_mpi" CACHE STRING "Name of the lammps library")
+    else()  # lammps_mpi is not available
+        set(LAMMPS_LIBRARY_NAME "lammps" CACHE STRING "Name of the lammps library")
+    endif()
+
+    # Try to find lammps using FindLAMMPS.cmake file. Custom because not included in cmake 3.29
     find_package(LAMMPS)
-    #set(LAMMPS_FLAGS "" CACHE STRING "necessary flags to include lammps")
-    #set(LAMMPS_PATH "" CACHE STRING "lammps library path, liblammps.so or liblammps.a")
     
-    #if (LAMMPS_PATH AND EXISTS ${LAMMPS_PATH})      # a valid path is given
-    #    message(STATUS "Using the given LAMMPS_PATH=${LAMMPS_PATH}")
-    #elseif (LAMMPS_PATH AND NOT EXISTS ${LAMMPS_PATH})
-    #    message(STATUS "The provided path does not exist: LAMMPS_PATH=${LAMMPS_PATH}")
-    #    message(STATUS "Please provide the full path to liblammps.so or liblammps.a with '-DLAMMPS_PATH=path/to/library/liblammps.so' including the file name or do not define LAMMPS_PATH to let pkg-config search for lammps.")
-    #elseif(${LAMMPS_FOUND}) # Find path automatically with pkg-config
-    #    set(LAMMPS_PATH_SHARED "${LAMMPS_LIBDIR}/lib${LAMMPS_LIBRARIES}.so")
-    #    set(LAMMPS_PATH_STATIC "${LAMMPS_LIBDIR}/lib${LAMMPS_LIBRARIES}.a")
-
-    #    if (EXISTS ${LAMMPS_PATH_SHARED})
-    #        message(STATUS "Found Lammps: ${LAMMPS_PATH_SHARED}")
-    #        set(LAMMPS_PATH ${LAMMPS_PATH_SHARED})
-    #    elseif (EXISTS ${LAMMPS_PATH_STATIC})
-    #        message(STATUS "Found Lammps: ${LAMMPS_PATH_STATIC}")
-    #        set(LAMMPS_PATH ${LAMMPS_PATH_STATIC})
-    #    else()
-    #        message(STATUS "Couldn't find liblammps.so or liblammps.a. Please provide the correct path with '-DLAMMPS_PATH=path/to/library/liblammps.so' including the file name.")
-    #    endif()
-    #else()
-    #    message(STATUS "Couldn't find lammps using pkg-config.")
-    #    message(STATUS "Please verify that lammps.pc can be found in PKG_CONFIG_PATH or provide the correct path to lammps with '-DLAMMPS_PATH=path/to/library/liblammps.so' including the file name.")
-    #endif()
-    
-    #add_library(lammps_library SHARED IMPORTED)
-    #set_target_properties(lammps_library PROPERTIES IMPORTED_LOCATION ${LAMMPS_PATH})
-    
-    #if(LAMMPS_FLAGS)
-    #    set_target_properties(lammps_library PROPERTIES SHARED_LIBRARY_FLAGS ${LAMMPS_FLAGS})
-    #elseif(${LAMMPS_FOUND})
-    #    message(STATUS "lammmmimmmps flags : ${LAMMPS_LDFLAGS}, et : ${LAMMPS_CFLAGS}")
-    #    set_target_properties(lammps_library PROPERTIES SHARED_LIBRARY_FLAGS "${LAMMPS_LDFLAGS}${LAMMPS_CFLAGS}")
-    #else()
-    #    message(STATUS "Couldn't find lammps using pkg-config.")
-    #    message(STATUS "Please verify that lammps.pc can be found in PKG_CONFIG_PATH or provide lammps flags with -DLAMMPS_FLAGS=\"-l<libraries>-L<link_directories>-D<VARIABLE>-I<include_directories>\" ")
-    #endif()
-
-    #set_target_properties(lammps_library PROPERTIES SHARED_LIBRARY_FLAGS "${LAMMPS_LDFLAGS}${LAMMPS_CFLAGS}")
-    #set_target_properties(lammps_library PROPERTIES SHARED_LIBRARY_FLAGS ${LAMMPS_FLAGS})
-    #target_link_libraries(lammps_library INTERFACE lammps)
-    #target_link_directories(lammps_library INTERFACE /home/catB/jd270899/.local/lib64)
-    #link_libraries( lammps_library )
-    set(${PROJECT_NAME}_LAMMPS_LIBRARIES ${LAMMPS_LIBRARIES})
-    set(${PROJECT_NAME}_LAMMPS_LIBRARY_DIRS ${LAMMPS_LIBRARY_DIRS})
-    set(${PROJECT_NAME}_LAMMPS_INCLUDE_DIR ${LAMMPS_INCLUDE_DIRS})
+    set(${PROJECT_NAME}_LAMMPS_LIBRARIES ${LAMMPS_LIBRARIES} PARENT_SCOPE)
+    set(${PROJECT_NAME}_LAMMPS_LIBRARY_DIRS ${LAMMPS_LIBRARY_DIRS} PARENT_SCOPE)
+    set(${PROJECT_NAME}_LAMMPS_INCLUDE_DIR ${LAMMPS_INCLUDE_DIRS} PARENT_SCOPE)
     
     list(APPEND LAMMPS_DEFINITIONS "LAMMPS_VERSION")
     list(APPEND LAMMPS_DEFINITIONS "LAMMPS_LIB_MPI")
-    Foreach( DEFINITION IN LAMMPS_DEFINITIONS)
-        list(APPEND ${PROJECT_NAME}_COMPILE_DEFINITION DEFINITION)
+    Foreach( DEFINITION IN ITEMS ${LAMMPS_DEFINITIONS})
+        list(APPEND ${PROJECT_NAME}_COMPILE_DEFINITION ${DEFINITION})
     endforeach()
-    
+    set(${PROJECT_NAME}_COMPILE_DEFINITION ${${PROJECT_NAME}_COMPILE_DEFINITION} PARENT_SCOPE)
 endfunction(compile_with_LAMMPS)
